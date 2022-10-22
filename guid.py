8bb3ed9a8b21a558fcb5dc60acb94225018d778a
Andzhey123 <beginov.a@gmail.com>
2022-10-22 16:31:57 +0300

    ДЗ7

---
+++ guid.py
@@ -0,0 +1,8 @@
+import div as div
+import mult as mul
+import subt as sub
+import sum as sum
+
+
+dict_ar = {'/': div, '*': mul, '-': sub, '+': sum}
+dict_log = {'/': 'деление', '*': 'умножение', '-': 'вычитание', '+': 'сложение'}
